c7fb76e6ec395c7e02bf9a6ed2d6d14108a1ae25
Emmett Keyser <emkeyser@gmail.com>
2017-04-09 12:11:37 -0700

    latest

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require("express");
-var router = express.Router();
+var router = new express.Router();
 
 router.get("/", function (req, res, next) {
+    "use strict";
+
     res.setHeader("content-type", "application/json");
-    res.send(JSON.stringify({version: "hello"}));
+    res.send(JSON.stringify({id: "sup bitch"}));
 });
 
 module.exports = router;
